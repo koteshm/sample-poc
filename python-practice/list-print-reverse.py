46f6ab0f7db1bc121dca1d3bf2a9c0acf2751620
kotesh <kotesh.matta@gmail.com>
2018-11-14 02:41:50 -0700

    python-practice

---
+++ python-practice/list-print-reverse.py
@@ -0,0 +1,7 @@
+import sys
+
+family = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
+index=len(family)-1
+while index>=0 :
+ print(family[index])
+ index -=1
---
+++ python-practice/list-sort-print.py
@@ -0,0 +1,6 @@
+import sys
+
+family = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
+family.sort()
+for name in family:
+ print(name)
